4521bd5ca0194c70a8c9f30c0e880e6e0c19ff11
Shane Smith <sshane@shanes-mbp.attlocal.net>
2023-05-17 16:28:27 -0400

    Added Laser Scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public int speed = 10;
+    public GameObject missle;
 
     // Start is called before the first frame update
     void Start()
@@ -13,13 +14,22 @@ public class PlayerMovement : MonoBehaviour
         transform.position = new Vector2(0, 0);
     }
 
+
     // Update is called once per frame
+
     void Update()
     {
-        
 
-       float hInput = Input.GetAxis("Horizontal");
-       float vInput = Input.GetAxis("Vertical");
+        CalculateMovement();
+        FireLaser();
+       
+    }
+
+
+    void CalculateMovement()
+    {
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
 
 
         Vector2 direction = new Vector2(hInput, vInput);
@@ -30,8 +40,14 @@ public class PlayerMovement : MonoBehaviour
 
         //transform.position = new Vector2(transform.position.x, Mathf.Clamp(transform.position.y, -8f, 8f));
         //transform.position = new Vector2(Mathf.Clamp(transform.position.x, -14f, 14), transform.position.y);
-        transform.position = new Vector2(Mathf.Clamp(transform.position.x, Camera.main.ViewportToWorldPoint(new Vector2(0,0)).x,Camera.main.ViewportToWorldPoint(new Vector2(1,0)).x),
-        Mathf.Clamp(transform.position.y, Camera.main.ViewportToWorldPoint(new Vector2(0,0)).y, Camera.main.ViewportToWorldPoint(new Vector2(0,1)).y));
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, Camera.main.ViewportToWorldPoint(new Vector2(0, 0)).x, Camera.main.ViewportToWorldPoint(new Vector2(1, 0)).x),
+        Mathf.Clamp(transform.position.y, Camera.main.ViewportToWorldPoint(new Vector2(0, 0)).y, Camera.main.ViewportToWorldPoint(new Vector2(0, 1)).y));
     }
-
+    void FireLaser()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(missle, transform.position, Quaternion.identity);
+        }
+    }
 }
